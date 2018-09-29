c2fb47304fdb6bea562746f308f171a648343283
Bruno Martins <brunomart.91@gmail.com>
2018-09-29 19:48:10 +0100

    Search bar WIP

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './SearchBar.css';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <input type="text" className="App-search" placeholder="Pick a game ..."/>
+        );
+    }
+}
