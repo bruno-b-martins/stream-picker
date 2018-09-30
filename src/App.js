4482bb8027c1fe5dc1890d07da8e5fd7327777b3
Bruno Martins <brunomart.91@gmail.com>
2018-09-30 12:34:55 +0100

    Add FontAwesome

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './assets/FontAwesome.js';
 import './App.css';
 import TwitchAPI from './services/TwitchAPI';
 import SearchBar from "./components/SearchBar";
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './SearchBar.css';
 
 class SearchBar extends Component {
     render() {
         return (
-            <input type="text" onChange={(text) => this.props.onChange(text)} className="App-search" placeholder="Pick a game ..."/>
+            <div className="SearchBar-container">
+                <input type="text" onChange={(text) => this.props.onChange(text)} className="App-search" placeholder="Pick a game ..."/>
+                <FontAwesomeIcon icon="search" />
+            </div>
         );
     }
 }
