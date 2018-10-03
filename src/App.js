19bea8553021b734169f466d427f19a6992a845d
Bruno Martins <brunomart.91@gmail.com>
2018-10-04 00:08:37 +0100

    Tried to implement unit tests with jest

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        let initialValues = this.loadInitialValues();
+        let initialValues = App.loadInitialValues();
 
         this.state = {
             getStreamsParams: {
@@ -54,7 +54,7 @@ class App extends Component {
      *
      * @returns {{numberOfResults: number}}
      */
-    loadInitialValues() {
+    static loadInitialValues() {
         let initialValues = {
             numberOfResults: 6
         };
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('app', () => {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('should load initial values', () => {
+        expect(App.loadInitialValues()).toBeTruthy();
+    });
+
 });
